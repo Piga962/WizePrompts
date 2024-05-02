674479f0c94f7fb3c5f3945a8b408c3d5a8b0e6d
Piga962 <cesarjrmecinas@gmail.com>
2024-05-01 19:24:36 -0600

    Update Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,42 @@
 import { useNavigate } from "react-router-dom";
 import "./Register.css";
+import logo from '../images/wizeline-sin-fondo.png';
 
 const Register = () => {
     const navigate = useNavigate();
     
     return (
-        <div className="Register">
-        <h1>Register</h1>
-        <button onClick={() => navigate("/Login")}>Login</button>
-        <button onClick={() => navigate("/")}>Home</button>
+        <div className="Register-container">
+            <div className="Heading">
+                <button className="Home-button" onClick={() => navigate("/Login")}>
+                    <img src={logo} alt="logo"/>
+                </button>
+                <h1>Register</h1>
+            </div>
+            <div className="Inputs">
+                <label className="input">
+                    <h2>Name</h2>
+                    <input className="input-text" type="text" name="name" placeholder="Name"/>
+                </label>
+                <label className="input">
+                    <h2>Email</h2>
+                    <input className="input-text" type="email" name="email" placeholder="Email"/>
+                </label>
+                <label className="input">
+                    <h2>Password</h2>
+                    <input className="input-text" type="password" name="password" placeholder="Password"/>
+                </label>
+                <label className="input">
+                    <h2>Confirm Password</h2>
+                    <input className="input-text" type="password" name="confirmPassword" placeholder="Password"/>
+                </label>
+                <label className="checkbox">
+                    <input className="input-checkbox" type="checkbox" name="terms"/>
+                    <span>I agree with the terms and conditions of Service</span>
+                </label>
+            </div>
+
+            <button className="Register-button" onClick={() => navigate("/Login")}>Register</button>
         </div>
     );
 };
